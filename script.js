b16357aafa7abde8bb646cfcd404e09a7eb0ca8c
RostoRM <92208542+RostoRM@users.noreply.github.com>
2023-02-15 20:53:45 +0400

    apikey change

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const loader = document.querySelector('.loader');
 
 // NASA API
 const count = 10;
-const apiKey = process.env.API_KEY;
+const apiKey = 'DEMO_KEY';
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
